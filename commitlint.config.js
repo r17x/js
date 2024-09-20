50bc60aee37346d4976c64adaadb15b6f40e19a2
r17x <ri7nz@evilfactory.id>
2024-09-20 18:20:10 +0700

    style: applied lint and formatter

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  extends: ["squash-pr", "@commitlint/config-conventional"],
-  ignores: [
-    // ignore lint commit when committed by release commit
-    (msg) => /^chore\((release|deps-dev|deps)?\):\s(@?\D+\d+.\d+.\d+|bump.*)\s(from|\[skip ci\]|to.*)?/g.test(msg),
-    // ignore lint commit when squash message
-    (msg) => /.*:.*\(#\d+\)/g.test(msg),
-  ],
+	extends: ["squash-pr", "@commitlint/config-conventional"],
+	ignores: [
+		// ignore lint commit when committed by release commit
+		(msg) =>
+			/^chore\((release|deps-dev|deps)?\):\s(@?\D+\d+.\d+.\d+|bump.*)\s(from|\[skip ci\]|to.*)?/g.test(
+				msg,
+			),
+		// ignore lint commit when squash message
+		(msg) => /.*:.*\(#\d+\)/g.test(msg),
+	],
 };
