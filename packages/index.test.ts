3b3baa1e43d9a453c0cb2b1dc9f55c5418e48307
r17x <ri7nz@evilfactory.id>
2024-09-22 15:46:48 +0700

    test: import package configuration

--- packages/index.test.ts
+++ packages/index.test.ts
@@ -1,7 +1,28 @@
+import * as fs from "node:fs";
 import { describe, expect, it } from "vitest";
 
 describe("vitest", () => {
 	it("works", () => {
 		expect(1).toBe(1);
+	});
+
+	it("biome configuration", () => {
+		expect(import("@r17x/biome").then((m) => m.default)).resolves.toStrictEqual(
+			JSON.parse(
+				fs.readFileSync("./packages/biome/index.json", { encoding: "utf8" }),
+			),
+		);
+	});
+
+	it("semantic-release configuration", () => {
+		expect(
+			import("@r17x/semantic-release").then((m) => m.default),
+		).resolves.toStrictEqual(
+			JSON.parse(
+				fs.readFileSync("./packages/semantic-release/index.json", {
+					encoding: "utf8",
+				}),
+			),
+		);
 	});
 });
